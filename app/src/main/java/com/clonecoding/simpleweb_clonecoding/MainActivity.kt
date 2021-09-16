9777097ea3b691a0fd4a3738771636e8dce04e2b
ujini1129 <ujini1129@gmail.com>
2021-09-16 12:06:37 +0900

    [main] git init

---
+++ app/src/main/java/com/clonecoding/simpleweb_clonecoding/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.clonecoding.simpleweb_clonecoding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
